b2bbe1f80a59609238d6b0ad062a11bd9acfa412
Parker <pp101socials@gmail.com>
2023-08-16 15:18:13 -0400

    Created file

---
+++ pages/pressure-washing/index.js
@@ -0,0 +1,18 @@
+// React/Next Imports
+import { useEffect } from "react";
+import { useRouter } from "next/router";
+
+// Library Imports
+
+// Data/Functions/Images Imports
+
+// Component Imports
+
+// Style Imports
+import "../assets/styles/modules/Sites/PressureWashing/PressureWashing.module.css";
+
+export default function PressureWashingIndex() {
+  const router = useRouter();
+
+  return "";
+}
