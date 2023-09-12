14d680c644533e3908cc0ece5632e04069633161
Parker <pp101socials@gmail.com>
2023-09-12 14:50:36 -0400

    Created file

---
+++ pages/lawn-care/about.js
@@ -0,0 +1,18 @@
+// React/Next Imports
+import { useEffect } from "react";
+import { useRouter } from "next/router";
+
+// Library Imports
+
+// Data/Functions/Images Imports
+
+// Component Imports
+
+// Style Imports
+import "../../assets/styles/modules/Sites/LawnCare/LawnCare.module.css";
+
+export default function LawnCareAbout() {
+  const router = useRouter();
+
+  return "";
+}
---
+++ pages/photography/contact.js
@@ -0,0 +1,18 @@
+// React/Next Imports
+import { useEffect } from "react";
+import { useRouter } from "next/router";
+
+// Library Imports
+
+// Data/Functions/Images Imports
+
+// Component Imports
+
+// Style Imports
+import "../../assets/styles/modules/Sites/Photography/Photography.module.css";
+
+export default function PhotographyContact() {
+  const router = useRouter();
+
+  return "";
+}
