1a2070a1ba90ae0f680ab63595fb1ede318d477d
Parker <pp101socials@gmail.com>
2023-09-12 14:36:55 -0400

    Added nav parameter

--- assets/functions/dom/closers/CloseMobileNav.js
+++ assets/functions/dom/closers/CloseMobileNav.js
@@ -2,8 +2,10 @@
  *
  *  This is to close the mobile nav
  *
+ *  @param {Object} nav - This is the nav for the specific website
+ *
  */
 
 import RemoveStorageVariable from "../../data/storage/RemoveStorageVariable";
 
-export default function CloseMobileNav() {}
+export default function CloseMobileNav(nav) {}
--- assets/functions/dom/togglers/ToggleMobileNav.js
+++ assets/functions/dom/togglers/ToggleMobileNav.js
@@ -2,8 +2,10 @@
  *
  *  This is used to toggle the mobile nav menu
  *
+ *  @param {Object} nav - This is the nav for the specific website
+ *
  */
 
 import DeclareStorageVariable from "../../data/storage/DeclareStorageVariable";
 
-export default function ToggleMobileNav() {}
+export default function ToggleMobileNav(nav) {}
