39ddac561bdd08a20014de3060340df597b931de
Parker <pp101socials@gmail.com>
2023-09-14 19:55:17 -0400

    Created file

---
+++ pages/api/trackers/Painting_trackIps.js
@@ -0,0 +1,22 @@
+import axios from "axios";
+
+// const IPSTACK_KEY = "6eafccee3db72681fcd3589b807c0f2e";
+
+// const fetchAWSIPRanges = async () => {
+//   try {
+//     const response = await axios.get(
+//       "https://ip-ranges.amazonaws.com/ip-ranges.json"
+//     );
+//     const data = response.data;
+//     return data.prefixes; // This contains the AWS IP address ranges
+//   } catch (error) {
+//     console.error("Error fetching AWS IP ranges:", error);
+//     return [];
+//   }
+// };
+
+export default async (req, res) => {
+  const message = "Hello World!";
+
+  res.status(200).json({ message });
+};
