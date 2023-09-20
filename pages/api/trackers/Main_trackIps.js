5745b64c39fd5b9a84fcfae0c5cf9b5ecc5393a8
Parker <pp101socials@gmail.com>
2023-09-20 05:36:49 -0400

    Changed ip pickup

--- pages/api/trackers/Main_trackIps.js
+++ pages/api/trackers/Main_trackIps.js
@@ -9,11 +9,15 @@ const fetchAWSIPRanges = async () => {
       "https://ip-ranges.amazonaws.com/ip-ranges.json"
     );
     const data = response.data;
-    return data.prefixes; // This contains the AWS IP address ranges
+    return data.prefixes.map((entry) => entry.ip.ip_prefix); // This contains the AWS IP address ranges
   } catch (error) {
     console.error("Error fetching AWS IP ranges:", error);
     return [];
   }
+};
+
+const isIPInExcludedRanges = (ip, excludedRanges) => {
+  return excludedRanges.some((range) => ip.startsWith(range));
 };
 
 export default async (req, res) => {
@@ -28,67 +32,104 @@ export default async (req, res) => {
   }
 
   try {
-    // Creating the connection
     const DB = await connectToDatabase();
-
-    // Creating the collection
     const IP_COLLECTION = DB.collection("ips");
 
-    // Check if the current IP address is already in the database.
-    const EXISTING_IP = await IP_COLLECTION.findOne({ IP_ADDRESS });
+    // Define the excluded IP ranges (AWS, GCP, Zscaler, Googlebot)
+    const excludedIPRanges = await fetchAWSIPRanges();
+    // You can add more excluded IP ranges if needed for other providers
 
-    console.log("IP_ADDRESS: " + IP_ADDRESS);
-
-    if (!EXISTING_IP) {
-      const currentDateTime = new Date().toISOString(); // Get the current date and time in ISO format
+    // Check if the current IP address is in the excluded ranges
+    if (!isIPInExcludedRanges(IP_ADDRESS, excludedIPRanges)) {
+      const currentDateTime = new Date().toISOString();
       await IP_COLLECTION.insertOne({
         IP_ADDRESS,
-        added_at: currentDateTime, // Add the timestamp property here
+        added_at: currentDateTime,
       });
-
-      //   // Make API request to IPStack to get location information
-      //   const LOCATION_INFO = await fetch(
-      //     `http://api.ipstack.com/${IP_ADDRESS}?access_key=6eafccee3db72681fcd3589b807c0f2e`
-      //   )
-      //     .then((response) => response.json())
-      //     .catch((error) => {
-      //       console.error(
-      //         `Error fetching location data for IP ${IP_ADDRESS}: ${error}`
-      //       );
-      //       return null; // Handles error gracefully
-      //     });
-
-      //   if (LOCATION_INFO) {
-      //     await IP_COLLECTION.updateOne(
-      //       { IP_ADDRESS },
-      //       { $set: { location: LOCATION_INFO } }
-      //     );
-      //   }
     }
 
-    // Removing null, Amazon Web Services (AWS) ip addresses
-    const AWS_IP_RANGES = await fetchAWSIPRanges();
+    // Remove null, Amazon Web Services (AWS) ip addresses, and others based on your criteria
     await IP_COLLECTION.deleteMany(
-      { IP_ADDRESS: { $in: AWS_IP_RANGES } },
-      { added_at: TARGET_ADDED_AT }
-    );
-    await IP_COLLECTION.deleteMany(
-      { IP_ADDRESS: null },
-      { added_at: TARGET_ADDED_AT }
-    );
-    await IP_COLLECTION.deleteMany(
-      { IP_ADDRESS: "::1" },
+      {
+        $or: [
+          { IP_ADDRESS: null },
+          { IP_ADDRESS: { $in: excludedIPRanges } },
+          // Add more conditions for other excluded IP ranges if needed
+        ],
+      },
       { added_at: TARGET_ADDED_AT }
     );
 
-    // Removing 3.238.66.225 ip (I have no idea what this is. I assume it might be Netlify)
-
-    // Getting the total count of unique IPs
     const TOTAL_UNIQUE_IPS = await IP_COLLECTION.countDocuments();
 
     res.status(200).json({ TOTAL_UNIQUE_IPS });
   } catch (error) {
     console.log("Error: " + error);
-    res.status(500).json({ error: "An error occured" });
+    res.status(500).json({ error: "An error occurred" });
   }
+
+  // try {
+  //   // Creating the connection
+  //   const DB = await connectToDatabase();
+
+  //   // Creating the collection
+  //   const IP_COLLECTION = DB.collection("ips");
+
+  //   // Check if the current IP address is already in the database.
+  //   const EXISTING_IP = await IP_COLLECTION.findOne({ IP_ADDRESS });
+
+  //   console.log("IP_ADDRESS: " + IP_ADDRESS);
+
+  //   if (!EXISTING_IP) {
+  //     const currentDateTime = new Date().toISOString(); // Get the current date and time in ISO format
+  //     await IP_COLLECTION.insertOne({
+  //       IP_ADDRESS,
+  //       added_at: currentDateTime, // Add the timestamp property here
+  //     });
+
+  //     //   // Make API request to IPStack to get location information
+  //     //   const LOCATION_INFO = await fetch(
+  //     //     `http://api.ipstack.com/${IP_ADDRESS}?access_key=6eafccee3db72681fcd3589b807c0f2e`
+  //     //   )
+  //     //     .then((response) => response.json())
+  //     //     .catch((error) => {
+  //     //       console.error(
+  //     //         `Error fetching location data for IP ${IP_ADDRESS}: ${error}`
+  //     //       );
+  //     //       return null; // Handles error gracefully
+  //     //     });
+
+  //     //   if (LOCATION_INFO) {
+  //     //     await IP_COLLECTION.updateOne(
+  //     //       { IP_ADDRESS },
+  //     //       { $set: { location: LOCATION_INFO } }
+  //     //     );
+  //     //   }
+  //   }
+
+  //   // Removing null, Amazon Web Services (AWS) ip addresses
+  //   const AWS_IP_RANGES = await fetchAWSIPRanges();
+  //   await IP_COLLECTION.deleteMany(
+  //     { IP_ADDRESS: { $in: AWS_IP_RANGES } },
+  //     { added_at: TARGET_ADDED_AT }
+  //   );
+  //   await IP_COLLECTION.deleteMany(
+  //     { IP_ADDRESS: null },
+  //     { added_at: TARGET_ADDED_AT }
+  //   );
+  //   await IP_COLLECTION.deleteMany(
+  //     { IP_ADDRESS: "::1" },
+  //     { added_at: TARGET_ADDED_AT }
+  //   );
+
+  //   // Removing 3.238.66.225 ip (I have no idea what this is. I assume it might be Netlify)
+
+  //   // Getting the total count of unique IPs
+  //   const TOTAL_UNIQUE_IPS = await IP_COLLECTION.countDocuments();
+
+  //   res.status(200).json({ TOTAL_UNIQUE_IPS });
+  // } catch (error) {
+  //   console.log("Error: " + error);
+  //   res.status(500).json({ error: "An error occured" });
+  // }
 };
